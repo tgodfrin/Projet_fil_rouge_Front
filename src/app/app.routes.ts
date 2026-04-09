c4d5aef217c0ea05d80f02a100a537f6fa87d3af
Thomas GODFRIN <thomas.godfrin98@gmail.com>
2026-04-09 11:08:39 +0200

    feat: configuration du routing principal avec routes enfants sous le LayoutComponent

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      }
+    ]
+  }
+];
